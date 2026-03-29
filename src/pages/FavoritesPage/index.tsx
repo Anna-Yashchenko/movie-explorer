a9685b338ce2c16970ca6d9e2249d931e5db1326
Anna Yashchenko <anna.yashchenko2001@gmail.com>
2026-03-29 22:06:48 +0300

    feat:favorites page has been implemented

--- src/pages/FavoritesPage/index.tsx
+++ src/pages/FavoritesPage/index.tsx
@@ -1,7 +1,17 @@
+import {useFavoritesStore} from "../../store/useFavoritesStore";
+import {MovieList} from "../../components/MovieList";
+
 export const FavoritesPage = () => {
+    const {favorites} = useFavoritesStore();
     return (
         <div>
-            <p>Избранное</p>
+            <h1>Избранное</h1>
+
+            {favorites.length === 0 ? (
+                <p>У вас пока нет избранных фильмов</p>
+            ) : (
+                <MovieList movies={favorites}/>
+            )}
         </div>
     )
 }
